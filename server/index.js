7b63bf2430128a9640fc9f141940846ac91c1da7
Tim Markin <timopro16@gmail.com>
2024-07-11 21:21:03 +0300

    update patreon api

--- server/index.js
+++ server/index.js
@@ -1,7 +1,15 @@
 require('dotenv').config();
-
 const express = require("express");
+const url = require('url');
+const patreon = require('patreon');
+const patreonAPI = patreon.patreon;
+const patreonOAuth = patreon.oauth;
 
+const CLIENT_ID = process.env.CLIENT_ID || 'KRiq_33aFb4Bltv-NydVH5KeWNTMzFS-05fesEZ-AguxO-EBuZLfmEvG-c5T1voJ';
+const CLIENT_SECRET = process.env.CLIENT_SECRET || 'Jw0mb7LxkLScY-fwlksVekR-REcjGeFh1t8pSf-d4xNKiKUOvxY6kumxAiif4xOd';
+const REDIRECT_URL = process.env.REDIRECT_URL || 'https://apiegames.com/patreon-api';
+
+const patreonOAuthClient = patreonOAuth(CLIENT_ID, CLIENT_SECRET);
 const PORT = process.env.PORT || 3001;
 
 const app = express();
@@ -10,40 +18,25 @@ app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
 });
 
+app.get("/patreon-oauth", (req, res) => {
+    const oauthGrantCode = url.parse(req.url, true).query.code;
+
+    patreonOAuthClient
+        .getTokens(oauthGrantCode, REDIRECT_URL)
+        .then(tokensResponse => {
+            const patreonAPIClient = patreonAPI(tokensResponse.access_token);
+            return patreonAPIClient('/current_user');
+        })
+        .then(result => {
+            const store = result.store;
+            res.json(store.findAll('user').map(user => user.serialize()));
+        })
+        .catch(err => {
+            console.error('error!', err);
+            res.status(500).send(err);
+        });
+});
+
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
-
-var url = require('url')
-var patreon = require('patreon')
-var patreonAPI = patreon.patreon
-var patreonOAuth = patreon.oauth
-
-// Use the client id and secret you received when setting up your OAuth account
-var CLIENT_ID = 'KRiq_33aFb4Bltv-NydVH5KeWNTMzFS-05fesEZ-AguxO-EBuZLfmEvG-c5T1voJ'
-var CLIENT_SECRET = 'Jw0mb7LxkLScY-fwlksVekR-REcjGeFh1t8pSf-d4xNKiKUOvxY6kumxAiif4xOd'
-var patreonOAuthClient = patreonOAuth(CLIENT_ID, CLIENT_SECRET)
-
-// This should be one of the fully qualified redirect_uri you used when setting up your oauth account
-var redirectURL = 'https://apiegames.com/patreon-api'
-
-function handleOAuthRedirectRequest(request, response) {
-    var oauthGrantCode = url.parse(request.url, true).query.code
-
-    patreonOAuthClient
-        .getTokens(oauthGrantCode, redirectURL)
-        .then(function(tokensResponse) {
-            var patreonAPIClient = patreonAPI(tokensResponse.access_token)
-            return patreonAPIClient('/current_user')
-        })
-        .then(function(result) {
-            var store = result.store
-            // store is a [JsonApiDataStore](https://github.com/beauby/jsonapi-datastore)
-            // You can also ask for result.rawJson if you'd like to work with unparsed data
-            response.end(store.findAll('user').map(user => user.serialize()))
-        })
-        .catch(function(err) {
-            console.error('error!', err)
-            response.end(err)
-        })
-}
