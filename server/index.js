d710dce2e92ffe1f35994f3fbb61af0be3860872
Tim Markin <timopro16@gmail.com>
2024-07-11 21:05:32 +0300

    modify patreon api

--- server/index.js
+++ server/index.js
@@ -1,73 +1,49 @@
 require('dotenv').config();
-const express = require('express');
-const axios = require('axios');
-const axiosRetry = require('axios-retry');
+
+const express = require("express");
 
 const PORT = process.env.PORT || 3001;
+
 const app = express();
 
-const PATREON_CLIENT_ID = process.env.PATREON_CLIENT_ID;
-const PATREON_CLIENT_SECRET = process.env.PATREON_CLIENT_SECRET;
-const PATREON_REDIRECT_URI = process.env.PATREON_REDIRECT_URI;
-
-// Configure axios to use axios-retry
-axiosRetry(axios, { retries: 3, retryDelay: (retryCount) => {
-    console.log(`Retry attempt: ${retryCount}`);
-    return retryCount * 2000; // time interval between retries
-}});
-
-app.get('/api', (req, res) => {
-    res.json({ message: 'Hello from server!' });
-});
-
-// Redirect users to Patreon for authorization
-app.get('/auth/patreon', (req, res) => {
-    const authUrl = `https://www.patreon.com/oauth2/authorize?response_type=code&client_id=${PATREON_CLIENT_ID}&redirect_uri=${encodeURIComponent(PATREON_REDIRECT_URI)}`;
-    res.redirect(authUrl);
-});
-
-// Handle the OAuth callback from Patreon
-app.get('/patreon-api', async (req, res) => {
-    const code = req.query.code;
-    if (!code) {
-        console.error('Code is missing');
-        return res.status(400).send('Code is missing');
-    }
-
-    try {
-        console.log(`Requesting token with code: ${code}`);
-        const tokenResponse = await axios.post('https://www.patreon.com/api/oauth2/token', null, {
-            params: {
-                code,
-                grant_type: 'authorization_code',
-                client_id: PATREON_CLIENT_ID,
-                client_secret: PATREON_CLIENT_SECRET,
-                redirect_uri: PATREON_REDIRECT_URI,
-            },
-        });
-
-        const accessToken = tokenResponse.data.access_token;
-        const refreshToken = tokenResponse.data.refresh_token;
-
-        console.log(`Access token: ${accessToken}`);
-        console.log(`Refresh token: ${refreshToken}`);
-
-        // Use the access token to fetch user data or other actions
-        const userResponse = await axios.get('https://www.patreon.com/api/oauth2/v2/identity', {
-            headers: {
-                Authorization: `Bearer ${accessToken}`,
-            },
-        });
-
-        console.log('User data retrieved successfully');
-        res.json(userResponse.data);
-    } catch (error) {
-        console.error('Error fetching access token', error.response ? error.response.data : error.message);
-        res.status(500).send('Internal Server Error');
-    }
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from server!" });
 });
 
 app.listen(PORT, () => {
-    console.log(`Server listening on ${PORT}`);
+  console.log(`Server listening on ${PORT}`);
 });
-//
+
+var url = require('url')
+var patreon = require('patreon')
+var patreonAPI = patreon.patreon
+var patreonOAuth = patreon.oauth
+
+// Use the client id and secret you received when setting up your OAuth account
+var CLIENT_ID = 'KRiq_33aFb4Bltv-NydVH5KeWNTMzFS-05fesEZ-AguxO-EBuZLfmEvG-c5T1voJ'
+var CLIENT_SECRET = 'Jw0mb7LxkLScY-fwlksVekR-REcjGeFh1t8pSf-d4xNKiKUOvxY6kumxAiif4xOd'
+var patreonOAuthClient = patreonOAuth(CLIENT_ID, CLIENT_SECRET)
+
+// This should be one of the fully qualified redirect_uri you used when setting up your oauth account
+var redirectURL = 'https://apiegames.com/patreon-api'
+
+function handleOAuthRedirectRequest(request, response) {
+    var oauthGrantCode = url.parse(request.url, true).query.code
+
+    patreonOAuthClient
+        .getTokens(oauthGrantCode, redirectURL)
+        .then(function(tokensResponse) {
+            var patreonAPIClient = patreonAPI(tokensResponse.access_token)
+            return patreonAPIClient('/current_user')
+        })
+        .then(function(result) {
+            var store = result.store
+            // store is a [JsonApiDataStore](https://github.com/beauby/jsonapi-datastore)
+            // You can also ask for result.rawJson if you'd like to work with unparsed data
+            response.end(store.findAll('user').map(user => user.serialize()))
+        })
+        .catch(function(err) {
+            console.error('error!', err)
+            response.end(err)
+        })
+}
